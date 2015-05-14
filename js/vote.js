8315086c27fcbc9fb87fa3043f928712ef4effd6
eidehua <edthemaster1@gmail.com>
2015-05-14 00:21:36 -0500

    Fixed upvote and downvote mechanics
    Can vote up, switch to other vote, and undo vote.

--- js/vote.js
+++ js/vote.js
@@ -10,44 +10,65 @@
     else{
       num = $(this).attr('id').slice(4); //slices first 4 letters (down), leaving the num
     }
-    var count = parseInt($("#count"+num).text());
+    //var count = parseInt($("#count"+num).text());
     var data = $("#data"+num);
 
     if($(this).hasClass("up")) {
 
-      var count = count + 1;
-      $("#up"+num).css('opacity','1');
-      $("#down"+num).css('opacity','0.5');
-      $('#up'+num).css('pointer-events','none');
-      $('#down'+num).css('pointer-events','auto');
-
-      $("#count"+num).text(count);
-
-      data.attr('upvotes', parseInt(data.attr('upvotes'))+1);
-      if(data.attr('votedOn')=="true"){ //if we have previously voted, must have been a downvote. Undo that.
-        data.attr('downvotes', parseInt(data.attr('downvotes'))-1)
+      //If we already voted up, and clicked vote up again, undo upvote
+      if(data.attr('votedUp')=='true'){
+        data.attr('upvotes',parseInt(data.attr('upvotes'))-1);
+        data.attr('votedUp', 'false');
+        $("#up"+num).css('opacity','0.5');
       }
+      else if(data.attr('votedUp')=='false'){
+        //So we have not voted up yet
+        data.attr('upvotes',parseInt(data.attr('upvotes'))+1);
+        data.attr('votedUp','true');
+        $("#up"+num).css('opacity','1');
+      }
+      //If we have already voted down
+      if(data.attr('votedDown')=='true'){
+        //undo downvote
+        data.attr('downvotes', parseInt(data.attr('downvotes'))-1);
+        data.attr('votedDown','false');
+        $("#down"+num).css('opacity','0.5');
+      }
+      //If we have not voted down, don't change downvotes.
+
       data.text('Upvotes: '+data.attr('upvotes')
         +' Downvotes: '+ data.attr('downvotes') + ' Confidence Score: ' + data.attr('confidence'));
 
     } else {
-      var count = count - 1;
-      $("#down"+num).css('opacity','1');
-      $('#up'+num).css('opacity',0.5);
-      $('#down'+num).css('pointer-events','none');
-      $('#up'+num).css('pointer-events','auto');
-      $("#count"+num).text(count);
 
-      data.attr('downvotes', parseInt(data.attr('downvotes'))+1);
-      if(data.attr('votedOn')=="true"){ //if we have previously voted, must have been an upvote. Undo that.
-        data.attr('upvotes', parseInt(data.attr('upvotes'))-1)
+      //If we already voted down, and clicked vote down again, undo downvote
+      if(data.attr('votedDown')=='true'){
+        data.attr('downvotes',parseInt(data.attr('downvotes'))-1);
+        data.attr('votedDown', 'false');
+        $("#down"+num).css('opacity','0.5');
       }
+      else if(data.attr('votedDown')=='false'){
+        //So we have not voted down yet
+        data.attr('downvotes',parseInt(data.attr('downvotes'))+1);
+        data.attr('votedDown','true');
+        $("#down"+num).css('opacity','1');
+      }
+      //If we have already voted up
+      if(data.attr('votedUp')=='true'){
+        //undo downvote
+        data.attr('upvotes', parseInt(data.attr('upvotes'))-1);
+        data.attr('votedUp','false');
+        $("#up"+num).css('opacity','0.5');
+      }
+
       data.text('Upvotes: '+data.attr('upvotes')
        +' Downvotes: '+ data.attr('downvotes') + ' Confidence Score: ' + data.attr('confidence'));
     }
-    if(data.attr('votedOn')=="false"){
-      data.attr('votedOn',"true");
-    }
+    //if(data.attr('votedOn')=="false"){
+      //data.attr('votedOn',"true");
+    //}
+    var count = parseInt(data.attr('upvotes')) - parseInt(data.attr('downvotes'));
+    $("#count"+num).text(count);
 
     updateDB(num);
   });
